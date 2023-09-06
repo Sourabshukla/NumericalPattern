d478eeb02381864e2bb5612b4192581e11ed40d7
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-09-06 19:19:25 +0530

    Update Fibonaci.java

--- Fibonaci.java
+++ Fibonaci.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Fibonaci {
 	static void fibo(int count) {
 		int n1=0,n2=1,n3=0,i;
-		System.out.print(n1+" "+n2+" ");
+		System.out.print(n1+" "+n2+" ");  // we know 1st 2 number will be 0 and 1
 		for(i=2;i<count;i++) {
 			n3=n1+n2;
 			System.out.print(n3+" ");
